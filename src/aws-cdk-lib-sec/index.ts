1c34c827bd0c9e844e33c991e1d208a49ec62b51
schadow98 <malteschadow@yahoo.de>
2025-03-16 20:52:47 +0100

    Stack-props

--- src/aws-cdk-lib-sec/index.ts
+++ src/aws-cdk-lib-sec/index.ts
@@ -1,6 +1,6 @@
 
 export * as logger from './SecLogger'
 export * from 'aws-cdk-lib'
-export { Stack } from './aws-stack'
+export { Stack, StackProps } from './aws-stack'
 export * as aws_lambda from './aws-lambda';
 export * as aws_signer from './aws-signer';
