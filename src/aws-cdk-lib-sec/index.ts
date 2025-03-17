b7c4f10a47287bde1b8b07b23a9f1637d3dfd5d1
schadow98 <malteschadow@yahoo.de>
2025-03-17 09:36:54 +0100

    module

--- src/aws-cdk-lib-sec/index.ts
+++ src/aws-cdk-lib-sec/index.ts
@@ -1,6 +1,17 @@
 
-export * as logger from './SecLogger'
-export * from 'aws-cdk-lib'
-export { Stack, StackProps } from './aws-stack'
-export * as aws_lambda from './aws-lambda';
-export * as aws_signer from './aws-signer';
+import * as logger from './SecLogger'
+import * as awsCdkLib from 'aws-cdk-lib'
+import { Stack, StackProps } from './aws-stack'
+import * as aws_lambda from './aws-lambda';
+import * as aws_signer from './aws-signer';
+
+
+
+module.exports = {
+  logger,
+  ...awsCdkLib,
+  Stack,
+  StackProps,
+  aws_lambda: aws_lambda,
+  aws_signer: aws_signer
+};
