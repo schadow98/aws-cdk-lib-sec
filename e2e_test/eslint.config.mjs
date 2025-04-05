29830bfe1f3f20285c61f2ef5e6d58b796e4de40
schadow98 <malteschadow@yahoo.de>
2025-04-05 13:47:26 +0200

    fix sast

--- e2e_test/eslint.config.mjs
+++ e2e_test/eslint.config.mjs
@@ -1,19 +1,27 @@
-import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
 import noSecrets from 'eslint-plugin-no-secrets';
 import security from 'eslint-plugin-security';
 
-export default [
+export default tseslint.config(
   {
-    ...js.configs.recommended,
-    files: ['src/**/*.ts', 'src/**/*.js'], // ← das ist wichtig!
+    files: ['src/**/*.ts'],
     ignores: ['jest.config.cjs'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: new URL('.', import.meta.url).pathname,
+        sourceType: 'module'
+      }
+    },
     plugins: {
+      '@typescript-eslint': tseslint.plugin,
       'no-secrets': noSecrets,
       security: security
     },
     rules: {
       'no-secrets/no-secrets': 'warn',
-      'security/detect-object-injection': 'warn',
+      'security/detect-object-injection': 'warn'
     }
   }
-];
+);
