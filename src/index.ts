8eb128f1db8ec01832b2cb3bb322cf0342c23b72
schadow98 <malteschadow@yahoo.de>
2025-04-05 19:28:52 +0200

    fix

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,13 @@
  * - Exposes the shared `logger` for consistent structured logging.
  */
 
+
 export * as logger from './SecLogger'
 export * from 'aws-cdk-lib'
 export { Stack, StackProps } from './aws-stack'
 export * as aws_lambda from './aws-lambda';
 export * as aws_signer from './aws-signer';
-
+export * as aws_apigateway from './aws-apigateway';
 
 
 
