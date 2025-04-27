a901d035d58907cb68dae622db26a28852c76fc0
schadow98 <malteschadow@yahoo.de>
2025-04-27 20:36:11 +0200

    fix test

--- performance_test/bin/performance_test.ts
+++ performance_test/bin/performance_test.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { PerformanceTestStack } from '../lib/performance_test-stack';
+import { MyLambdaApiStack } from '../lib/performance_test-stack';
 
 const app = new cdk.App();
-new PerformanceTestStack(app, 'PerformanceTestStack', {
+new MyLambdaApiStack(app, 'MyLambdaApiStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
